a04df8b434db2af7394d5159aebb28cd541cf168
AmFlint <amasselot@hotmail.com>
2017-12-27 19:22:25 +0100

    Add proper test script and folder architecture for helpers

--- tests/initial_test/chakram.js
+++ tests/initial_test/chakram.js
@@ -1,9 +1,6 @@
 const chakram = require('chakram');
 const expect = chakram.expect;
 
-const url = process.env.DNS;
-console.log(process.env.DNS);
-
 describe("Chakram", function() {
     it("should offer simple HTTP request capabilities", function () {
         return chakram.get("http://httpbin.org/get");
