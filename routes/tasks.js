61eda84409f094c227ab64eb729eadbb5453c23b
AmFlint <amasselot@hotmail.com>
2018-02-26 20:17:37 +0100

    Create routes Listing/Create subtasks + Create function for task routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,53 +1,56 @@
-const express = require('express'),
-      router = express.Router(),
-      Task = require('../models/task');
+const Task = require('../models/task');
 
 // TODO: Implement board notion to tasks management.
+router = function(server) {
 
-// Create a Task
-router.post('/', function(req, res, next) {
-   const data = req.body;
-   const task = new Task(data);
-   task.validate()
-       .then(() => {
-           task.save()
-               .then(task => res.send(200, task))
-               .catch(err => res.send(500, error));
-       })
-       .catch(err => res.send(400, err));
-});
+    // Create a Task
+    server.post('/boards/:boardId/tasks/', function(req, res, next) {
+        const data = req.body;
+        const task = new Task(data);
+        task.validate()
+            .then(() => {
+                task.save()
+                    .then(task => res.send(200, task))
+                    .catch(err => res.send(500, err));
+            })
+            .catch(err => res.send(400, err));
+    });
 
 // List tasks
-router.get('/', function(req, res, next) {
-   Task.find().exec()
-       .then(tasks => res.status(200).send(tasks))
-       .catch(err => res.status(500).send(err));
-});
+    server.get('/boards/:boardId/tasks/', function(req, res, next) {
+        Task.find().exec()
+            .then(tasks => res.status(200).send(tasks))
+            .catch(err => res.status(500).send(err));
+    });
 
 // Get single task
-router.get('/:id', function(req, res, next) {
-    Task.findOne({_id: req.params.id})
-        .then((task) => {
-            if (task) {
-                res.status(200).send(task)
-            }
-            res.status(404).send({message: 'No task found', status: 404});
-        })
-        .catch(err => res.status(404).send(err));
-});
+    server.get('/boards/:boardId/tasks/:id', function(req, res, next) {
+        console.log(req);
+        Task.findOne({_id: req.params.id})
+            .then((task) => {
+                if (task) {
+                    res.status(200).send(task);
+                    res.end();
+                }
+                res.status(404).send({message: 'No task found', status: 404});
+            })
+            .catch(err => res.status(404).send(err));
+    });
 
 // Delete a task
-router.delete('/:id', function(req, res, next) {
-    Task.findOne({_id: req.params.id})
-        .then(task => {
-            if (!task) {
-                res.status(404).send({message: 'Task not found', status: 404});
-            }
-            task.remove()
-                .then(task => res.status(200).send(task))
-                .catch(err => res.status(500).send(err));
-        })
-        .catch(err => res.status(500).send(err));
-});
+    server.delete('/boards/:boardId/tasks/:id', function(req, res, next) {
+        Task.findOne({_id: req.params.id})
+            .then(task => {
+                if (!task) {
+                    res.status(404).send({message: 'Task not found', status: 404});
+                }
+                task.remove()
+                    .then(task => res.status(200).send(task))
+                    .catch(err => res.status(500).send(err));
+            })
+            .catch(err => res.status(500).send(err));
+    });
+
+};
 
 module.exports = router;
