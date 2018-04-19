1182626cae84d8d835d078501aba7e085c1fe4c4
amasselot <amasselot@online.net>
2018-04-19 14:32:36 +0200

    [Testing] Remove comment on Health test

--- tests/health_test/health.js
+++ tests/health_test/health.js
@@ -6,14 +6,14 @@ const chai = require('chai'),
 chai.use(chaiHttp);
 
 describe('/health', function () {
-    // it('It should return a 200 response code', function (done) {
-    //     chai.request(server)
-    //         .get('/health')
-    //         .end((err, response) => {
-    //             response.should.have.status(200);
-    //             response.body.should.have.property('healthy');
-    //             response.body.healthy.should.be.equal(true);
-    //             done();
-    //         });
-    // })
+    it('It should return a 200 response code', function (done) {
+        chai.request(server)
+            .get('/health')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.have.property('healthy');
+                response.body.healthy.should.be.equal(true);
+                done();
+            });
+    })
 });
